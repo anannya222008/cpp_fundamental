3228b922c999ddb43d17b54ad52a97c0c906bbda
anannya <anannya222008@gmail.com>
2026-06-03 18:42:00 +0530

    Fix voting eligibility file and add even or odd program

--- c++ basics/01_basics_and_conditionals/08_voting_eligiblity.cpp
+++ c++ basics/01_basics_and_conditionals/08_voting_eligiblity.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
+    int age;
     cout<<"Enter your age:";
-    cin>>n;
-    if(n>=18){
+    cin>>age;
+    if(age>=18){
         cout<<"You are eligible to vote."<<endl;
     }else{
         cout<<"You are not eligible to vote."<<endl;
         return 0;
     }
+    
 }
 
