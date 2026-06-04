a8d566dec9e52d2be1198a6e76f3754b0e3bec9c
anannya <anannya222008@gmail.com>
2026-06-04 17:29:20 +0530

    modified:   c++ basics/01_basics_and_conditionals/10_user_input_grade.cpp
    	new file:   c++ basics/01_basics_and_conditionals/11_character_type_check.cpp

--- c++ basics/01_basics_and_conditionals/10_user_input_grade.cpp
+++ c++ basics/01_basics_and_conditionals/10_user_input_grade.cpp
@@ -6,14 +6,14 @@ int main() {
     cin>>marks;
     if(marks>=90){
         cout<<"Grade A"<<endl;   
-     }else if(marks>=80 && marks<90){
+    }else if(marks>=80 && marks<90){
         cout<<"Grade B"<<endl;
-         }else if(marks>=70 && marks<80){
+    }else if(marks>=70 && marks<80){
         cout<<"Grade C"<<endl;
-         }else if(marks>=60 && marks<70){
+    }else if(marks>=60 && marks<70){
         cout<<"Grade D"<<endl;
-         }else{
+    }else{
         cout<<"Grade F"<<endl;
-         }
+    }
 
 }
