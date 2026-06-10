efab6dbea84462523879ad6c4a82012cffb2c7b0
anannya <anannya222008@gmail.com>
2026-06-10 17:33:51 +0530

    uploading pattern problems

--- c++ basics/02_loops_function_pattern/07_square_pattern.cpp
+++ c++ basics/02_loops_function_pattern/07_square_pattern.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 using namespace std;
-int main() {
+int main(){
     int n;
     cout<<"Enter a number: ";
     cin>>n;
-    for(int i= 1;i<=n;i++){
-        
+    for(int i=1;i<=n;i++){
+
         for(int j=1;j<=n;j++){
             cout<<"*";
+
         }
         cout<<endl;
     }
     
     return 0;
 }
+    
+    
