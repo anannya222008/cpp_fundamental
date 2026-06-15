8411f2033fac3f6b49f094392b10e88cd300b3f7
anannya <anannya222008@gmail.com>
2026-06-15 15:31:40 +0530

    uploading triangle pattern problem codes

---
+++ c++ basics/02_loops_function_pattern/17_inverted_triangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    for(int i=n;i>0;i--){
+        for(int j=1;j<i;j++){
+            cout<<j<<" ";
+
+        }
+        cout<<endl;
+         
+    }
+    
+    
+        
+    
+    
+}
+        
