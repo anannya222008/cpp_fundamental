3864e5c895feef472a83480dfb20c02ba2a1fd14
anannya <anannya222008@gmail.com>
2026-06-22 14:53:38 +0530

    uploading pattern problem code

--- c++ basics/02_loops_function_pattern/30_down_arrow.cpp
+++ c++ basics/02_loops_function_pattern/30_down_arrow.cpp
@@ -5,17 +5,14 @@ int main(){
     cout<<"Enter a number : ";
     cin>>n;
    for(int i=0;i<n;i++){
-    for(int j=0;j<i;j++){
-        cout<<" ";
+     for(int j=0;j<i;j++){
+         cout<<" ";
+        }
+        cout<<"*";
+        for(int j= 2*(n-i)-5;j>0;j--){
+            cout<<" ";
+        }
+        cout<<"*";
+        cout<<endl;
     }
-    cout<<"*";
-    if(i!=n-1){
-    for(int j=(2*n-3)-2*i;j>0;j--){
-        cout<<" ";
-    }
-    cout<<"*";
-    }
-     
-    cout<<endl;
-   }
 }
