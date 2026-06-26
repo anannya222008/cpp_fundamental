cb78223f7a9d3007de59f35bf4b1c352aabfd1e6
anannya <anannya222008@gmail.com>
2026-06-26 17:17:54 +0530

    code

---
+++ c++ basics/02_loops_function_pattern/41_binary_1_to_10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int convert(int deciNum){
+    int ans = 0,power = 1;
+    while(deciNum > 0){
+        int rem = deciNum%2;
+        deciNum = deciNum/2;
+        ans += (rem * power);
+        power *=10;
+
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cout<<"Enter a decimal number : ";
+    cin>>n;
+    for(int i=1;i<=10;i++){
+        cout<<convert(i)<<endl;
+    }
+    cout<<convert(n)<<endl;
+    return 0;
+}
+    
