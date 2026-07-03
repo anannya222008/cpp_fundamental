e60ece3552b8d7681136d4b6ee3261877156e38f
anannya <anannya222008@gmail.com>
2026-07-03 16:05:57 +0530

    understanding scope of variables.

---
+++ c++ basics/02_loops_function_pattern/49_scope.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int a = 4;
+int num(){
+    int b = 10;
+    return 0;
+}
+int main(){
+    cout<<a<<endl; // 4
+    cout<<b<<endl; // error: 'b' was not declared in this scope
+}
+
