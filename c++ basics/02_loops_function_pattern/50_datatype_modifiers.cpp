900cfb98695989821c6240718de704d80a6cf918
anannya <anannya222008@gmail.com>
2026-07-04 15:56:48 +0530

    .

--- c++ basics/02_loops_function_pattern/50_datatype_modifiers.cpp
+++ c++ basics/02_loops_function_pattern/50_datatype_modifiers.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a;
-    cout<<"Enter a number: ";
-    cin>>a;
-    cout<< sizeof(a)<<endl; // 4
-    long long b;
-    cout<<"Enter a number: ";
-    cin>>b;
-    cout<< sizeof(b)<<endl; // 8
+    cout<<sizeof(long int)<<endl; // 8
+    cout<< sizeof(long long int)<<endl; // 8
+    cout<< sizeof(long long)<<endl; // 8
+    cout<< sizeof(short)<<endl; // 2
+    cout<< sizeof(char)<<endl; // 1
+    cout<< sizeof(float)<<endl; // 4
+    cout<< sizeof(double)<<endl; // 8
 }
