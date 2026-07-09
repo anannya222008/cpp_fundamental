96946ad20a97a6f20f24a2dd426b82fe9381ebee
anannya <anannya222008@gmail.com>
2026-07-09 21:19:28 +0530

    arrays syntax

--- 03_dsa/01_intro_array.cpp
+++ 03_dsa/01_intro_array.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int marks[5] = {100, 90, 80, 70, 60};
+    int marks[5] = {100, 90, 80, 70, 60};// size of array is 5
     double price[] = {10.5, 20.5, 30.5, 40.5, 50.5};// size of array is 5
+    return 0;
 }
