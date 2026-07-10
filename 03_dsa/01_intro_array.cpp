5128f24fadfa5a50a8fec24a2057070da6c9fb70
anannya <anannya222008@gmail.com>
2026-07-10 19:51:22 +0530

    updating code

--- 03_dsa/01_intro_array.cpp
+++ 03_dsa/01_intro_array.cpp
@@ -3,5 +3,7 @@ using namespace std;
 int main(){
     int marks[5] = {100, 90, 80, 70, 60};// size of array is 5
     double price[] = {10.5, 20.5, 30.5, 40.5, 50.5};// size of array is 5
+    cout<<marks[0]<<endl;// prints 100
+    cout<<price[0]<<endl;// prints 10.5
     return 0;
 }
