e613a54f19a2c96d22ddf1c47f3f39fa1489ba48
anannya <anannya222008@gmail.com>
2026-07-13 18:42:11 +0530

    updating code

--- 03_dsa/largest_num_array.cpp
+++ 03_dsa/largest_num_array.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int nums[] = {3,29,7,-23,-3};
     int size = sizeof(nums)/sizeof(nums[0]);
-    int largest = -INT_MAX;
+    int largest = INT_MIN;
     for(int i=0;i<size;i++){
         if(nums[i]>largest){
             largest = nums[i];
