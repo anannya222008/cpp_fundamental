59d7b6b0c2974b6b7ca6f924811e327abe6341e3
anannya <anannya222008@gmail.com>
2026-07-15 15:16:15 +0530

    reverse an array

---
+++ 03_dsa/reverseAnArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int reverse(int arr[],int size){
+    int start = 0,end = size - 1;
+    while(start < end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+
+}
+int main(){
+    int array[] = {2,44,2,6,8,54};
+    int size = sizeof(array)/sizeof(array[0]);
+    reverse(array , size);
+    for(int i=0;i<size;i++){
+        cout << array[i] << " ";
+    }
+    return 0;
+}
