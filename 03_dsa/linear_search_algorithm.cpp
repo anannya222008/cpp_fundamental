6d316ffc5ae93e4f91daa497df2beb1a50b47e11
anannya <anannya222008@gmail.com>
2026-07-15 14:50:28 +0530

    linear seach algorithm

--- 03_dsa/linear_search_algorithm.cpp
+++ 03_dsa/linear_search_algorithm.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int linearSearch(int arr[],int size,int target){
     for(int i=0;i<size;i++){
         if(arr[i] == target){
-            return i;
+            return i;//found the target element, return its index
         }
     }
-    return -1;
+    return -1;//target element not found, return -1
 }
 int main(){
     int array[] = {1,2,3,4,5,6,7,8,9};
