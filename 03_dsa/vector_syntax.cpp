5b550a86c332e82c8da8cb5eae0b0ddef099a81a
anannya <anannya222008@gmail.com>
2026-07-19 16:32:46 +0530

    updating code

--- 03_dsa/vector_syntax.cpp
+++ 03_dsa/vector_syntax.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main(){
     vector <int> vec;// Declare a vector of integers
     cout<<sizeof(vec)<<endl;// Print the size of the vector object in bytes
-    return 0;
+    vector <int> vec1 = {1,2,3,4};// Declare a vector of integers with 4 elements initialized to 1,2,3,4
+    cout<<sizeof(vec1)<<endl;// Print the size of the vector object in bytes
+
 }
