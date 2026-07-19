2ca59545704932438da82ea761a79f0460aa3a9d
anannya <anannya222008@gmail.com>
2026-07-19 16:29:38 +0530

    updating code

--- 03_dsa/vector_syntax.cpp
+++ 03_dsa/vector_syntax.cpp
@@ -3,5 +3,6 @@
 using namespace std;
 int main(){
     vector <int> vec;// Declare a vector of integers
+    cout<<sizeof(vec)<<endl;// Print the size of the vector object in bytes
     return 0;
 }
