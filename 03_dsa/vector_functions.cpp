4a57d65e595eb61623deeb14803129bad4baf9a2
anannya <anannya222008@gmail.com>
2026-07-20 16:37:37 +0530

    learning functions of vectors

---
+++ 03_dsa/vector_functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int main(){
+vector<int> vec = {1,2,3,4};
+cout<<vec.size()<<endl;// Print the size of the vector
+vec.push_back(5);// Add an element to the end of the vector
+for(int i : vec){
+    cout<< i<<" ";
+}
+ cout<<endl;
+cout<<vec.size()<<endl;
+}
