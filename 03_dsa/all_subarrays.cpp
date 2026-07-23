5590a184f6405f9445fd8fd2e70c06a49ee48565
anannya <anannya222008@gmail.com>
2026-07-23 17:39:57 +0530

    updating code

--- 03_dsa/all_subarrays.cpp
+++ 03_dsa/all_subarrays.cpp
@@ -6,9 +6,9 @@ int main(){
     int sz = 5;
     int array[] = {1, 2, 3, 4, 5};
 
-    for(int start = 0;start <sz;start++){
-        for(int end = start;end<sz;end++){
-            for(int i=start;i<=end;i++){
+    for(int st = 0;st <sz;st++){
+        for(int end = st;end<sz;end++){
+            for(int i=st;i<=end;i++){
                 cout<<array[i];
             }
             cout<<" ";
