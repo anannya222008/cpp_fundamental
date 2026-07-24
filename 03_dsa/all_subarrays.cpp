fbf191139e321c001e1461a40acdbd6d150ef471
anannya <anannya222008@gmail.com>
2026-07-24 16:11:48 +0530

    updating code

--- 03_dsa/all_subarrays.cpp
+++ 03_dsa/all_subarrays.cpp
@@ -5,14 +5,12 @@ using namespace std;
 int main(){
     int sz = 5;
     int array[] = {1, 2, 3, 4, 5};
-
-    for(int st = 0;st <sz;st++){
+    for(int st = 0; st <sz;st++){
         for(int end = st;end<sz;end++){
             for(int i=st;i<=end;i++){
                 cout<<array[i];
             }
             cout<<" ";
-            
         }
         cout<<endl;
     }
