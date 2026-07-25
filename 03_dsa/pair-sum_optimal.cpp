f8e4eba0507f2d45409470e67f5247080721cdc7
anannya <anannya222008@gmail.com>
2026-07-25 17:27:20 +0530

    finding pair whose sum gives target

--- 03_dsa/pair-sum_optimal.cpp
+++ 03_dsa/pair-sum_optimal.cpp
@@ -4,6 +4,30 @@ using namespace std;
 
 vector <int> pairSum(vector <int>& nums,int target){
     int sz = nums.size();
+    vector <int> ans;
     int i = 0,j = sz-1; //two pointers
-    
+    while(i < j){
+        int sum = nums[i] + nums[j];
+        if(sum > target){
+            j--;
+        }else if(sum < target){
+            i++;
+        }else{
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);  
+            return ans;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector <int> nums = {-3,1,4,6,9};
+    int target = 5;
+    vector <int> vec = pairSum(nums,target);
+    cout<<"The nums whose sum gives " <<target<<" are : ";
+    for(int i:vec){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return 0;
 }
