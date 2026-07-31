9ae6c16440b3b49c36660a8cb1075aa56c40f2fc
anannya <anannya222008@gmail.com>
2026-07-31 16:45:16 +0530

    updating code

--- 03_dsa/pow(x,n)_binaryExpo.cpp
+++ 03_dsa/pow(x,n)_binaryExpo.cpp
@@ -3,6 +3,27 @@
 #include <algorithm>
 using namespace std;
 
-int pow(double x,long n){
-    
+double pow(double x,long n){
+    long long biNum = n;
+    if(n == 0) return 1;
+    if(x == 0) return 0;
+    if(x == 1) return 1;
+    if(biNum<0){
+        x = 1/x;
+        biNum = -biNum;
+    }
+    double ans = 1.0;
+    while(biNum>0){
+        if(biNum%2 == 1){
+            ans *= x;
+        }
+        x *= x;
+        biNum /= 2;
+
+    }
+    return ans;
 }
+int main(){
+    cout<<pow(0,-2)<<endl;
+}
+ 
