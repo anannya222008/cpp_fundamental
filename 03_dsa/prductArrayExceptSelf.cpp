6a74646b3cdfe3e87db6831c56e9d61d8d13a351
anannya <anannya222008@gmail.com>
2026-08-03 18:19:43 +0530

    product of array except self

---
+++ 03_dsa/prductArrayExceptSelf.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector <int> product(vector <int>&nums){
+    int sz = nums.size();
+    vector <int>ans(sz,1);
+    vector <int>prefix(sz,1);
+    vector <int>suffix(sz,1);
+    for(int i=1;i<sz;i++){
+        prefix[i] = prefix[i-1]*nums[i-1];
+    }
+    for(int i=sz-2;i>=0;i--){
+        suffix[i] = suffix[i+1]*nums[i+1];
+    }
+    for(int i=0;i<sz;i++){
+        ans[i]=prefix[i]*suffix[i];
+    }
+    return ans;
+}
+int main(){
+    vector <int>nums = {2,1,3,6,3};
+    vector <int>ans = product(nums);
+    for(int i:nums){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    for(int i:ans){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
