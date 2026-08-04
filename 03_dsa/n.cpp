ab775c5bf6a653378297e72c5fb353aaad7c205a
anannya <anannya222008@gmail.com>
2026-08-04 19:51:35 +0530

    contains duplicate

---
+++ 03_dsa/n.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool containsDuplicate(vector<int>& nums) {
+    int sz = nums.size();
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<sz-1;i++){
+        if(nums[i] == nums[i+1]){
+            return true;
+        }
+    }
+    return false;
+}
+int main(){
+    vector<int>nums = {1,2,3,4}; 
+    bool ans = containsDuplicate(nums);
+    cout<<ans<<endl;
+}
+
