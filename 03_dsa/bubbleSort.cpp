141dee4a61e7728349fa37277578254e63c4a1c0
anannya <anannya222008@gmail.com>
2026-08-14 17:32:48 +0530

    updating code

--- 03_dsa/bubbleSort.cpp
+++ 03_dsa/bubbleSort.cpp
@@ -5,11 +5,16 @@ using namespace std;
 
 vector<int> bubbleSort(vector<int>& nums){
     int n = nums.size();
+    bool isSwap = false;
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){   //O(n^2)
             if(nums[j]>nums[j+1]){
                 swap(nums[j],nums[j+1]);
+                isSwap = true;
             }
+        }
+        if(!isSwap){//there is no sawp,its already sorted.
+            return;
         }
     }
     return nums;
